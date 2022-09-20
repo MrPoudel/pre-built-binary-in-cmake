07dc11cc3dc8e209e5afd748e434ea72eee8a332
Bal Krishna Poudel <bal.krishna.poudel1@cariad.technology>
2022-09-20 11:50:58 +0200

    added files

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include "square.h"
+
+
+int main()
+
+{
+
+  int i;
+
+  printf("Enter the number:\n");
+
+  scanf("%d",&i);
+
+  //Function to calculate square of a number
+
+  f_square(i);
+
+  return 0;
+
+}
